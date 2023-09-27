d4d7df2d9ef246cd0b641b4104d067a57597c2ac
mustafaevf <knetya274@gmail.com>
2023-09-27 12:17:54 +0300

    fix

--- lr1.cpp
+++ lr1.cpp
@@ -1,132 +1,152 @@
 ﻿#include <iostream>
-#include <vector>
+#include <thread>
+#include <fstream>
 #include <string>
-
+#include <vector>
 
 using namespace std;
 
 
+vector<unsigned char> stringToUnsignedChar(const string& key) {
+	vector<unsigned char> ucharVector;
 
-vector <vector<unsigned char>> generateTable() {
-	vector<vector<unsigned char>> result (26, vector<unsigned char> (26));
-	
+	for (char c : key) {
+		ucharVector.push_back(static_cast<unsigned char>(c));
+	}
+
+	return ucharVector;
+}
+
+vector<vector<unsigned char>> getTable(){
+	vector<vector<unsigned char>> result(256, vector<unsigned char>(256));;
 	int k = 0;
 	int k1 = 0;
 
-	for(int i = 97; i < 122; i++) 
+	for (int i = 0; i < 256; i++)
 	{
-		for (int j = 97; j < 122; j++)
+		for (int j = 0; j < 256; j++)
 		{
-			if (k == 26)
+			if (k == 256)
 				k = 0;
 			result[i][j] = k;
 			k++;
 		}
 		k1++;
 		k = k1;
-	
 	}
-	
-
 	return result;
 }
 
-
-string decode(string text, string key, const vector<vector<unsigned char>> &table) {
-	string result = "";
-
-	vector<int> text_arr;
-	vector<int> key_arr;
-
-
-	for (unsigned char ch : text)
-		text_arr.push_back((int)ch);
-
-	for (unsigned char ch : key)
-		key_arr.push_back((int)ch);
-
-	int i = 0;
-
-	while (key_arr.size() != text_arr.size()) {
-		key_arr.push_back(key_arr[i]);
-		i++;
+void decode(unsigned char& symbol, unsigned char& key_s) {
+	ofstream file("decode.txt", ios::app);
+	if (!file.is_open()) {
+		cout << "File error opened";
+		return;
 	}
+	vector<vector<unsigned char>> table = getTable();
 	
-	for (int i = 0; i < key_arr.size(); i++) {
+	int key_position;
+	int symbol_position;
+	for (int j = 0; j < 256; j++) {
+		if ((int)table[(int)key_s][j] == (int)symbol) {
+			symbol_position = j;
+		}
+	}
+
+	file << table[0][symbol_position];
+}
+
+void encode(unsigned char& symbol, unsigned char& key_s) {
+	ofstream file("result.txt", ios::app);
+	if (!file.is_open()) {
+		cout << "File error opened";
+		return;
+	}
+	vector<vector<unsigned char>> table = getTable();
+	char result;
+	int symbol_position;
+	int key_position;
+	for (int j = 0; j < 256; j++) {
+		if ((int)table[0][j] == (int)symbol) {
+			symbol_position = j;
+			
+		}
+	}
+	for (int i = 0; i < 256; i++) {
+		if ((int)table[i][0] == (int)key_s) {
+			key_position = i;
+			
+		}
+	}
+
+
+	file << table[key_position][symbol_position];
+}
+
+
+void getSymbolFromFile(const string& filename, string& key, void(*f)(unsigned char&, unsigned char&)) {
+	unsigned char symbol;
+	ifstream file(filename, ios::binary);
+	file >> noskipws; 
+
+	if (!file.is_open()) {
+		cout << "File error opened";
+		return;
+	}
+	file >> noskipws;
+	int i = 0;
+	vector<unsigned char>key_vector = stringToUnsignedChar(key);
+	while (!file.eof()) {
+		file >> symbol;
+		//cout << symbol;
 		
-		for (int k = 0; k < table.size(); k++) {
-			int key_pos = 0;
-			if (key_arr[i] == table[k][0]) {
-				key_pos = k;
-				for (int j = 0; j < table[0].size(); j++) {
-					if (text_arr[i] == table[key_pos][j]) {
-						result += table[0][j];
-					}
-
-				}
-			}
-				
-		}
-	}
-
-
-	return result;
-}
-
-string encode(string text, string key, const vector<vector<unsigned char>> &table) {
-	string result = "";
-	vector<int> text_arr;
-	vector<int> key_arr;
-	for (unsigned char ch : text)
-		text_arr.push_back((int)ch);
-
-	for (unsigned char ch : key)
-		key_arr.push_back((int)ch);
-
-	int i = 0;
-	while (key_arr.size() != text_arr.size()) {
-		key_arr.push_back(key_arr[i]);
+		//static_cast<unsigned char>(key_s);
+		f(symbol, key_vector[i]);
 		i++;
+		if (key_vector.size() == i)
+			i = 0;
 	}
-	
-	for (int i = 0; i < text_arr.size(); i++) {
-		int symbol = text_arr[i];
-		int symbol_pos = 0;
-		int key = key_arr[i];
-		int key_pos = 0;
-		for (int k = 1; k < table[0].size(); k++) {
-			if (symbol == table[0][k])
-				symbol_pos = k;
-		}
-		for (int k = 1; k < table.size(); k++) {
-			if (key == table[k][0])
-				key_pos = k;
-		}
-		result += table[key_pos][symbol_pos];
-	}
-	
-	return result;
+	cout << "Cool";
+	return;
 }
 
 
 int main(int argc, char argv[])
 {
 	setlocale(0, "rus");
-	/*cout << "Выберите действие" << endl;
-	cout << "1. Зашифровать" << endl;
-	cout << "2. Расшифровать" << endl;
-	for (;;) {
-		int menu_point;
-		cin >> menu_point;
-	}*/
-	vector<vector<unsigned char>> table = generateTable();
-	/*int i = 0;
-	for (auto ch : ) {
-		cout << ch[i];
-		i++;
-	}*/
-	cout << encode("abcd", "abcd", table) << endl;
-	cout << decode("abcd", "abcd", table) << endl;
-	
-}
 
+	cout << "1) Зашифровать\n2) Расшифровать\n3) Взломать ключ" << endl;
+	int point;
+	for (;;) {
+		cout << "Выберите: ";
+		cin >> point;
+		if (point == 1) {
+			string filename;
+			string key;
+			cout << "Введите название файла, который надо зашифровать: ";
+			cin >> filename;
+			cout << "Введите ключ, которым надо зашифровать: ";
+			cin >> key;
+			getSymbolFromFile(filename, key, encode);
+		}
+		else if (point == 2) {
+			string filename;
+			string key;
+			
+			cout << "Введите название файла, который надо расшифровать: ";
+			cin >> filename;
+			cout << "Введите ключ, которым надо расшифровать: ";
+			cin >> key;
+			getSymbolFromFile(filename, key, decode);
+		}
+		else if (point == 3) {
+			int sub_point;
+			cin >> sub_point;
+		}
+		else {
+			cout << "1 или 2\n";
+		}
+	}
+
+	return 0;
+}
